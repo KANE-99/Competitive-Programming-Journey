c0d4ddb26879bfef3bf43704238990d5b81e6b0e
Kirtesh Suthar <42038491+KANE-99@users.noreply.github.com>
2020-08-14 17:21:58 +0530

    Create countAndSay.cpp
    Link to problem - https://leetcode.com/explore/interview/card/top-interview-questions-easy/127/strings/886/

---
+++ LeetCode/countAndSay.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    string countAndSay(int n) {
+        
+        if (n == 1) {
+            return "1";
+        }
+        
+        string fres="",res = countAndSay(n - 1);
+        int len = res.size();
+        if (len == 1) {
+            return "11";
+        }
+        
+        map<char, int> count;
+        
+        for (int i=1; i<len; i++) {
+            if (res[i] == res[i-1]) {
+                count[res[i]]++;
+            }else{
+                fres += to_string(count[res[i-1]]+1)+res[i-1];
+                count[res[i-1]] = 0;
+            }
+        }
+        fres += to_string(count[res[len-1]]+1)+res[len-1];
+        return fres;
+    }
+};
